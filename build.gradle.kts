0441bf1d1e49bd377f2490e86d08c4f1500cfb5a
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2025-03-28 17:04:31 +0530

    Update build.gradle.kts1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("org.sonarqube") version "4.4.1.3373"
 }
+sonarqube {
+    properties {
+        property("sonar.projectKey", "majid9971_majidApplication")
+        property("sonar.organization", "majid9971")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
